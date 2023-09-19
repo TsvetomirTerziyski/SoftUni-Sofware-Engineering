0d8f301af51415a533976122e96f731160599dea
TsvetomirTerziyski <141222857+TsvetomirTerziyski@users.noreply.github.com>
2023-09-19 17:46:02 +0300

    Delete data_types_and_variables_lab directory

--- data_types_and_variables_lab/next_happy_year.py
+++
@@ -1,9 +0,0 @@
-year = int(input())
-
-while True:
-    year += 1
-
-    if len(set(str(year))) == len(list(str(year))):
-        break
-
-print(year)
--- data_types_and_variables_lab/special_numbers.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-
-for number in range(1,n+1):
-    list_of_digits = [int(num) for num in str(number)]
-    if sum(list_of_digits) == 5 or sum(list_of_digits) == 7 or sum(list_of_digits) == 11:
-        print(f'{number} -> {True}')
-    else:
-        print(f'{number} -> {False}')
