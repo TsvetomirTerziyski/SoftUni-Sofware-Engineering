290438ce9c9f1bf4405e0b4ad19a8f7910ccff52
TsvetomirTerziyski <141222857+TsvetomirTerziyski@users.noreply.github.com>
2023-09-19 17:45:53 +0300

    Delete dictionaries_exercise directory

--- dictionaries_exercise/capitals.py
+++
@@ -1,7 +0,0 @@
-country_names = input().split(', ')
-capitals = input().split(', ')
-
-dictionary = {country_names[index]: capitals[index] for index in range(len(country_names))}
-
-for country, capital in dictionary.items():
-    print(f'{country} -> {capital}')
